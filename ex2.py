9f6ef4618c36304863b1ca35e8702cdf81a02732
noatal345 <noatal345@gmail.com>
2023-05-14 15:42:47 +0300

    fix

--- ex2.py
+++ ex2.py
@@ -1,3 +1,5 @@
+from random import random
+import random
 
 
 def create_english_dictionary(filename):
@@ -7,7 +9,7 @@ def create_english_dictionary(filename):
     with open(filename) as f:
         for line in f:
             english_dict.add(line.strip())
-    #close file
+    # close file
     f.close()
     return english_dict
 
@@ -20,6 +22,8 @@ def create_dictionary(filename):
     # open the filename and read each line.
     with open(filename) as f:
         for line in f:
+            if line == "\t\n" or line == "\n" or line == "\t":
+                break
             # if the line is empty or the line doesn't contain a "\t" char continue to the next line.
             if "\t" not in line:
                 continue
@@ -27,6 +31,8 @@ def create_dictionary(filename):
             value, key = line.split("\t")
             # remove the "\n" from the key.
             key = key.strip()
+            # convert the value to float.
+            value = float(value)
             # add the key and value to the dictionary.
             new_dict[key] = value
     f.close()
@@ -34,20 +40,12 @@ def create_dictionary(filename):
 
 
 def count_words(filename):
-    characters = set()
-    characters.add(".")
-    characters.add(",")
-    characters.add(" ")
-    characters.add("\n")
-    characters.add(";")
-    new_word_delimiter = set()
-    new_word_delimiter.add(" ")
-    new_word_delimiter.add("\n")
-    numer_of_words = 0
-    number_of_char = 0
-    special_char = 0
+    # create a set of characters that are not part of a word.
+    characters = {".", ",", " ", "\n", ";"}
+    # set of word delimiters
+    new_word_delimiter = {" ", "\n"}
+    numer_of_words, special_char, total_chars = 0, 0, 0
     flag_word = False
-    total_chars = 0
     # open filename and count the number characters and the number of words in the file.
     with open(filename) as f:
         for line in f:
@@ -69,21 +67,40 @@ def count_words(filename):
     return numer_of_words, number_of_char
 
 
+# def init_first_generation(num_of_strings, length, legal_characters):
+#     # create a list of strings
+#     list_of_strings = []
+#
+#     # create num_of_strings strings with length characters each of permutations of legal_characters
+#     for i in range(num_of_strings):
+#         new_string = ""
+#         for j in range(length):
+#             # choose a random character from legal_characters
+#             char = random.choice(legal_characters)
+#             new_string += char
+#
+#
+
 
 
 # the main program
 # create a dictionary of english words according to the dictionary file.
-english_dict = create_dictionary("dict.txt")
+english_dict = create_english_dictionary("dict.txt")
 num_of_words, num_of_chars = count_words("enc.txt")
 print("number of chars = ", num_of_chars)
 print("number of words = ", num_of_words)
 a = create_dictionary("Letter2_Freq.txt")
-print(a.items())
-print(a.keys())
-print(a.values())
 b = create_dictionary("Letter_Freq.txt")
+print("len(a)")
+print(len(a))
+print("len(b)")
+print(len(b))
+print(a.items())
 print(b.items())
-print(b.keys())
-print(b.values())
 
-print("hadar")
+
+
+abc_dictionary = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'k', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z'}
+number_of_strings = 32
+length_of_string = 26
+# init_first_generation(number_of_strings, length_of_string, abc_dictionary)
